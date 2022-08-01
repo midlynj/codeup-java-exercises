67d7ce5db4e31eb5dfce0723532ad1ccb12d29f9
midlynj <jmidlyn@gmail.com>
2022-08-01 16:03:50 -0500

    java primitive type and unary exercise complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,37 @@
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello, World!");
+        int myFavoriteNumber = 10;
+        System.out.println(myFavoriteNumber);
+        float myNumber = (float) 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(y);
+        System.out.println(y);
+
+
+    }
 }
