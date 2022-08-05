2f80064ca7a3eeca028f0a0e55bab6569510936d
midlynj <jmidlyn@gmail.com>
2022-08-05 13:49:33 -0500

    factorial exercise complete

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -8,7 +8,8 @@ public class MethodsExercises {
 //        mathMultiplication(2,2);
 //        mathModulus(2,2);
 //        getInteger(1,10);
-        factorial(1,10);
+//        factorial(1,10);
+        rollDice();
     }
         public static void mathAddition ( int input1, int input2){
             System.out.println(input1 + input2);
@@ -46,19 +47,7 @@ public class MethodsExercises {
             System.out.print("Good job you did it \uD83D\uDE4C \uD83E\uDD29 ");
             return userInteger;
         }
-//        boolean notANumber = true;
-//        while (notANumber) {
-//            if (scanner.hasNextInt()) {
-//                int userNumber = scanner.nextInt();
-//                notANumber = false;
-//            } else if (scanner.hasNext()){
-////                System.out.print("Try again: ");
-//                String userNumber = scanner.nextLine();
-//                notANumber = true;
 //
-//            }
-//        }
-//        System.out.print("Good job you did it \uD83D\uDE4C \uD83E\uDD29 ");
 
 
 
@@ -70,30 +59,49 @@ public class MethodsExercises {
         System.out.print("Enter an integer from 1 to 10");
         int userInteger = scanner.nextInt();
 
-//        for (int i = 1; i <= userInteger; i++) {
-//            System.out.println(i  + " * " + i  + " = " + i * i);
-//        }
-
-
-//        String[] abcvd = {"1","2","3","4","5","6","7","8","9","10"};
-
+//
         long fact = 1;
         for(int i = 1; i <= userInteger; i++)
         {
             fact = fact * i;
-//            System.out.println(abcvd[i]);
+
             System.out.printf("%s x ", i);
         }
         System.out.println(" = " +fact);
-//        System.out.println(abcvd[i]);
-//        }
+
         System.out.println("Wanna play again? [y/n]");
         String playAgain = scanner.next();
         if (playAgain.equals("y")) {
             factorial(min,max);
         }
     }
+
+    public static void rollDice() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of sides for a pair of dice");
+        int userDiceFace = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Roll dice? [y/n]");
+        String rollDice = scanner.next();
+        if (rollDice.equals("y")) {
+            System.out.println( userDiceFace + " sided Dice Rolled");
+            int diceRolledOnce = (int)(Math.random() * userDiceFace);
+            int diceRolledTwice = (int)(Math.random() * userDiceFace);
+            System.out.println("You rolled a " + diceRolledOnce);
+            System.out.println("You rolled a " + diceRolledTwice);
+            System.out.print("Would you like to play again? [y/n]");
+            String playAgain = scanner.next();
+                if (playAgain.equals("y")) {
+                    rollDice();
+                } else {
+                    System.out.print("Thanks for playing. Goodbye");
+                }
+
+
+        }
+        System.out.println("BYE!!!");
     }
-//}
+    }
+
 
 
